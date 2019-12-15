44544677cd5096d86834f581ce37d188de99b97d
Kirill <dukennt@gmail.com>
2019-12-15 23:21:54 +0300

    commit#2 add config.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config, ProdConfig, DevConfig
 
 app = Flask(__name__)
+
+app.config.from_object('config.DevelopConfig')
 
 @app.route('/hi')
 def hello():
---
+++ config.py
@@ -0,0 +1,9 @@
+class Config(object):
+	DEBUG = False
+
+
+class ProdConfig(Config):
+	DEBUG = False
+
+class DevConfig(Config):
+	DEBUG = True
